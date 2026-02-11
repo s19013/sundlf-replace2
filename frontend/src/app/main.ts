8aa776057099306b905de8d7074ab8beecbf755d
s19013 <hideya670@gmail.com>
2026-02-11 22:07:49 +0900

    primevueを使えるように

--- frontend/src/app/main.ts
+++ frontend/src/app/main.ts
@@ -6,9 +6,21 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config'
+// まだどれが良いか決めかねるから一旦全部出しとく
+import Aura from '@primeuix/themes/aura'
+// import Material from '@primeuix/themes/material'
+// import Lara from '@primeuix/themes/lara'
+// import Nora from '@primeuix/themes/nora'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
 
 app.mount('#app')
