ce3bc331aa36abdbf7f804d3d6ea5ec075f8a87d
s19013 <hideya670@gmail.com>
2026-02-12 20:43:02 +0900

    レート制限追加

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:60,1');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
