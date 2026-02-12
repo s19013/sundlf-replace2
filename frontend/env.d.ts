0bb932c4dad62fa2e37dfae60f115c38f79c4f38
s19013 <hideya670@gmail.com>
2026-02-12 14:15:09 +0900

    typescript用の型がない。なので手動で定義するしかない。

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+
+// このパッケージにはtypescript用の型がない。なので手動で定義するしかない。
+declare module '@jamescoyle/vue-icon' {
+  import type { DefineComponent } from 'vue'
+
+  const SvgIcon: DefineComponent<{
+    type?: string
+    path: string
+    size?: string | number
+    viewbox?: string
+    flip?: 'horizontal' | 'vertical' | 'both' | 'none'
+    rotate?: number
+  }>
+
+  export default SvgIcon
+}
