6769e9d37a81adefded41f77d531b91ab87a3096
s19013 <hideya670@gmail.com>
2026-02-13 18:53:12 +0900

    typecheckを自動実行するlefthook定義

---
+++ .lefthook/pre-commit/typecheck.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# bashで実行
+
+# | オプション         | 意味                 |
+# | ------------- | ------------------ |
+# | `-e`          | コマンドが1つでも失敗したら即終了  |
+# | `-u`          | 未定義変数を使ったらエラー      |
+# | `-o pipefail` | パイプの途中で失敗してもエラーにする |
+
+set -euo pipefail
+
+# lefthook から渡された staged_files が $@ に入る
+# 引数でファイルが渡されなかったら即終了
+if [[ $# -eq 0 ]]; then
+  exit 0
+fi
+
+# /frontend/パスと渡されるので ./パス となるように編集
+# 配列 files に格納
+files=()
+for f in "$@"; do
+files+=("${f/#frontend\//./}")
+done
+
+# ${files[@]} Bash 配列を 安全に展開
+mise exec:vue "pnpm run type-check-lefthook ${files[@]} > typecheck-error.log"
