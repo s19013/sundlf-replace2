8fd2eaa3ea2dbe900a1b320db9fa52cdcb1c5b7d
s19013 <hideya670@gmail.com>
2026-02-13 22:22:36 +0900

    ziggyのrouteのラッパークラスを作成

---
+++ frontend/src/shared/api/ziggyRoute.ts
@@ -0,0 +1,15 @@
+import type { RouteList, RouteParams } from 'ziggy-js'
+import { route } from 'ziggy-js'
+import { Ziggy } from '@/shared/api/ziggy'
+
+// ziggy-jsの内部型 RouteName と同等（exportされていないため再定義）
+// これにより、ideなどで補完が効くようになる。
+type RouteName = keyof RouteList | (string & {})
+
+export const ziggyRoute = <T extends RouteName>(
+  name: T,
+  params?: RouteParams<T>,
+  absolute?: boolean,
+) => {
+  return route(name, params, absolute, Ziggy)
+}
