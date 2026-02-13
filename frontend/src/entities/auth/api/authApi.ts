4d26fe2bccf3d5b437b8e9e45a9eb3143d2dc715
s19013 <hideya670@gmail.com>
2026-02-13 20:53:20 +0900

    ziggyを使ってapiのurlを呼び出すように変更

--- frontend/src/entities/auth/api/authApi.ts
+++ frontend/src/entities/auth/api/authApi.ts
@@ -1,22 +1,30 @@
-import { apiClient, webClient } from '@/shared/api'
+import { apiClient } from '@/shared/api'
 import type { LoginForm, User } from '../types/auth'
+import { route } from 'ziggy-js'
+import { Ziggy } from '@/app/ziggy'
 
 export function getCsrfCookie() {
   // SPAを認証するには、SPAの「ログイン」ページで最初に/sanctum/csrf-cookieエンドポイントにリクエストを送信して、アプリケーションのCSRF保護を初期化する必要ある。
-  // しかし、/sanctum/csrf-cookie は先頭にapiをつけないのでwebClientを使う必要がある。
-  return webClient.get(`/sanctum/csrf-cookie`)
+  return apiClient.get(route('sanctum.csrf-cookie', undefined, undefined, Ziggy))
 }
 
+console.log(route('spa.login', undefined, undefined, Ziggy))
+
 export async function login(credentials: LoginForm): Promise<User> {
-  const response = await apiClient.post<{ user: User }>('/spa/login', credentials)
+  const response = await apiClient.post<{ user: User }>(
+    route('spa.login', undefined, undefined, Ziggy),
+    credentials,
+  )
   return response.data.user
 }
 
 export function logout() {
-  return apiClient.post('/spa/logout')
+  return apiClient.post(route('spa.logout', undefined, undefined, Ziggy))
 }
 
 export async function getCurrentUser(): Promise<User> {
-  const response = await apiClient.get<{ user: User }>('/user')
+  const response = await apiClient.get<{ user: User }>(
+    route('auth.user', undefined, undefined, Ziggy),
+  )
   return response.data.user
 }
