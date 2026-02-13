a699b79f7d9f831ef72f5becbe6759fb857c063e
s19013 <hideya670@gmail.com>
2026-02-13 22:38:18 +0900

    デバックようにゆるくしてたのを戻す

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Auth\SPAAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/spa')->name('spa.')->group(function () {
-    Route::post('/login', [SPAAuthController::class, 'login'])->middleware('throttle:100,1')->name('login');
+    Route::post('/login', [SPAAuthController::class, 'login'])->middleware('throttle:10,1')->name('login');
     Route::post('/logout', [SPAAuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
 });
 
