2fdc48041a2a1b83e4c3f44976d5501b3904e406
s19013 <hideya670@gmail.com>
2026-02-13 18:13:05 +0900

    ルートネームを設定

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,13 +3,13 @@
 use App\Http\Controllers\Auth\SPAAuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/spa')->group(function () {
-    Route::post('/login', [SPAAuthController::class, 'login'])->middleware('throttle:10,1');
-    Route::post('/logout', [SPAAuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::prefix('/spa')->name('spa.')->group(function () {
+    Route::post('/login', [SPAAuthController::class, 'login'])->middleware('throttle:100,1')->name('login');
+    Route::post('/logout', [SPAAuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [SPAAuthController::class, 'user']);
+    Route::get('/user', [SPAAuthController::class, 'user'])->name('auth.user');
 
     Route::get('/test/auth', function () {
         return response()->json(['message' => 'api auth test']);
