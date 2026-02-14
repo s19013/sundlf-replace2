6224dd57cb930cccc5d876b1eab1d2075aec35f5
s19013 <hideya670@gmail.com>
2026-02-14 23:57:17 +0900

    ziggyにより"バックのurl/api/~"の状態で出力されるのでbaseurl自体がいらなくなった。

--- frontend/src/shared/api/apiClient.ts
+++ frontend/src/shared/api/apiClient.ts
@@ -1,17 +1,10 @@
 import axios from 'axios'
 
-// ziggyにより/apiがついた状態で出力されるのでbaseurlに"/api"はつけない
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
-
-if (!apiBaseUrl) {
-  console.error('環境変数 VITE_API_BASE_URL が設定されていません')
-  throw new Error('環境変数 VITE_API_BASE_URL が設定されていません')
-}
+// note:ziggyにより"バックのurl/api/~"の状態で出力されるのでbaseurl自体がいらなくなった。
 
 export const apiClient = axios.create({
   withCredentials: true,
   withXSRFToken: true,
-  baseURL: apiBaseUrl,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json;charset=utf-8',
