70e77eec2cb9f61293a1872a57090fb31a073880
s19013 <hideya670@gmail.com>
2026-02-23 00:38:08 +0900

    基本的なmdiアイコンボタンのコンポーネント作成

---
+++ frontend/src/shared/ui/MdiButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Button } from 'primevue'
+
+defineProps({
+  icon: {
+    type: String,
+    default: null,
+  },
+  label: {
+    type: String,
+    default: null,
+  },
+  gap: {
+    type: Number,
+    default: 1,
+  },
+})
+</script>
+
+<template>
+  <Button>
+    <div :class="`flex gap-${gap}`">
+      <SvgIcon type="mdi" :path="icon" />
+      <p>{{ label }}</p>
+    </div>
+  </Button>
+</template>
--- frontend/src/shared/ui/index.ts
+++ frontend/src/shared/ui/index.ts
@@ -1,1 +1,2 @@
 export { default as OriginalFooter } from './originalFooter.vue'
+export { default as MdiButton } from './MdiButton.vue'
