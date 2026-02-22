f2f49ceb943b9ad97fa34116cc8e8dd91dd4ff6f
s19013 <hideya670@gmail.com>
2026-02-22 23:50:10 +0900

    メッセージを表示するとこまで作成

---
+++ frontend/src/pages/welcom/WelcomView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import WelcomMessages from './ui/WelcomMessages.vue'
+</script>
+
+<template>
+  <img class="logo" src="/sundlf_logo.png" alt="ロゴ" />
+  <div class="mt-8 overflow-hidden shadow sm:rounded-lg">
+    <h3 class="p-3">
+      メモ､ブックマークにタグを付けて保存して整理､検索などで探しやすくするツールアプリです
+    </h3>
+    <WelcomMessages />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  width: 25rem;
+}
+</style>
