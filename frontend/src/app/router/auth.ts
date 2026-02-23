f620c93159754aa32d41825efcde7054646435cc
s19013 <hideya670@gmail.com>
2026-02-23 18:35:08 +0900

    コンテキストキーワードと衝突するらしいので変更

--- frontend/src/app/router/auth.ts
+++ frontend/src/app/router/auth.ts
@@ -1,18 +1,18 @@
 import type { RouteRecordRaw } from 'vue-router'
 
-const as = 'auth'
-const prefix = `/${as}`
+const section = 'auth'
+const prefix = `/${section}`
 
 export const authRoutes: Array<RouteRecordRaw> = [
   {
     path: `${prefix}/register`,
-    name: `${as}.register`,
+    name: `${section}.register`,
     component: () => import('@/pages/auth/register/RegisterView.vue'),
     meta: { guestOnly: true },
   },
   {
     path: `${prefix}/login`,
-    name: `${as}.login`,
+    name: `${section}.login`,
     component: () => import('@/pages/auth/login/LoginView.vue'),
     meta: { guestOnly: true },
   },
