4b5fc6812682bf2784f5458d1333ba0789b565aa
s19013 <hideya670@gmail.com>
2026-02-23 15:19:50 +0900

    新規ページを追加

--- frontend/src/app/router/index.ts
+++ frontend/src/app/router/index.ts
@@ -5,6 +5,16 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      name: 'welcome',
+      component: () => import('@/pages/welcome/WelcomeView.vue'),
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/pages/auth/register/RegisterView.vue'),
+    },
+    {
       path: '/login',
       name: 'login',
       component: () => import('@/pages/auth/login/LoginView.vue'),
