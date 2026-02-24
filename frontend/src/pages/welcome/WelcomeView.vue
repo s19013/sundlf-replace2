94f43f4a27c0008b461b6a4bba182ece21ba2149
s19013 <hideya670@gmail.com>
2026-02-24 16:35:03 +0900

    レイアウト適応

--- frontend/src/pages/welcome/WelcomeView.vue
+++ frontend/src/pages/welcome/WelcomeView.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
+import GuestLayout from '@/shared/layout/GuestLayout.vue'
 import ButtonsComponent from './ui/ButtonsComponent.vue'
 import WelcomeMessages from './ui/WelcomeMessages.vue'
 </script>
 
 <template>
-  <img class="logo" src="/sundlf_logo.png" alt="ロゴ" />
-  <div class="mt-8 overflow-hidden shadow sm:rounded-lg">
-    <h3 class="p-3">
-      メモ､ブックマークにタグを付けて保存して整理､検索などで探しやすくするツールアプリです
-    </h3>
-    <WelcomeMessages />
-  </div>
-  <ButtonsComponent />
+  <GuestLayout>
+    <img class="logo" src="/sundlf_logo.png" alt="ロゴ" />
+    <div class="mt-8 overflow-hidden shadow sm:rounded-lg">
+      <h3 class="p-3">
+        メモ､ブックマークにタグを付けて保存して整理､検索などで探しやすくするツールアプリです
+      </h3>
+      <WelcomeMessages />
+    </div>
+    <ButtonsComponent />
+  </GuestLayout>
 </template>
 
 <style scoped>
