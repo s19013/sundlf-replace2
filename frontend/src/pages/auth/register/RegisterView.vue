8a64210fd8052d49df0a5bb0658786b0a351a52b
s19013 <hideya670@gmail.com>
2026-02-24 16:37:14 +0900

    mdiButtonに変更

--- frontend/src/pages/auth/register/RegisterView.vue
+++ frontend/src/pages/auth/register/RegisterView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 // import { useRouter } from 'vue-router'
-import { InputText, Password, Button, Message } from 'primevue'
+import { InputText, Password, Message } from 'primevue'
 import { useAuthStore } from '@/entities/auth/model/authStore'
 import { AuthLayout } from '@/shared/layout'
+import { MdiButton } from '@/shared/ui'
+import { mdiAccountPlus } from '@mdi/js'
 
 // const router = useRouter()
 const authStore = useAuthStore()
@@ -80,9 +82,11 @@ async function handleRegister() {
         />
       </div>
 
-      <Button
-        type="submit"
+      <MdiButton
+        style="background-color: #eeeeee"
+        :icon="mdiAccountPlus"
         label="登録"
+        type="submit"
         :loading="authStore.isLoading"
         :disabled="authStore.isLoading"
         fluid
