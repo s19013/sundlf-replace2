672bd68cb9897f8e07ca2f04bf8faf1b6a98586f
s19013 <hideya670@gmail.com>
2026-02-25 11:05:51 +0900

    大きさ調節可能 ラベルとチェックボックスを揃える

--- frontend/src/shared/ui/CheckBox.vue
+++ frontend/src/shared/ui/CheckBox.vue
@@ -4,15 +4,20 @@ import { Checkbox } from 'primevue'
 // 勝手に一番上のdivにもattributeが付くのを防止
 defineOptions({ inheritAttrs: false })
 
+// note:公式の型自体がこうなっている
+type Size = 'small' | 'large' | undefined
+
 const props = withDefaults(
   defineProps<{
     label: string
     id?: string
     fluid?: boolean | null
+    size?: Size
   }>(),
   {
     id: undefined,
     fluid: true,
+    size: undefined,
   },
 )
 
@@ -21,19 +26,13 @@ const inputId = computed(() => props.id ?? generatedId)
 </script>
 
 <template>
-  <div :class="{ fluid: fluid }">
-    <Checkbox size="large" :inputId="inputId" v-bind="$attrs" binary />
+  <div class="mb-5 flex items-center gap-2" :class="{ fluid: fluid }">
+    <Checkbox :size="size" :inputId="inputId" v-bind="$attrs" binary />
     <label :for="inputId">{{ label }}</label>
   </div>
 </template>
 
 <style scoped>
-div {
-  margin-bottom: 1.25rem;
-  display: flex;
-  gap: 1rem;
-}
-
 .fluid {
   width: 100%;
 }
