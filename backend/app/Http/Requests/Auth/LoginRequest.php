89d29795520165828e5ac6a1e17cadbadcb64763
s19013 <hideya670@gmail.com>
2026-02-26 11:04:07 +0900

    php はbookean

--- backend/app/Http/Requests/Auth/LoginRequest.php
+++ backend/app/Http/Requests/Auth/LoginRequest.php
@@ -19,7 +19,7 @@ class LoginRequest extends FormRequest
         return [
             'email' => ['required', 'email'],
             'password' => ['required', 'string'],
-            'remember' => ['bool'],
+            'remember' => ['boolean'],
         ];
     }
 }
