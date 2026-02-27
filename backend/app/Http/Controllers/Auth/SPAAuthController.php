6624f9baf1bb68c63839cf5e5d965cb2594e5603
s19013 <hideya670@gmail.com>
2026-02-27 17:39:04 +0900

    新規登録機能生成

--- backend/app/Http/Controllers/Auth/SPAAuthController.php
+++ backend/app/Http/Controllers/Auth/SPAAuthController.php
@@ -4,14 +4,21 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
 use App\Usecases\Auth\SPA\GetCurrentUserUsecase;
 use App\Usecases\Auth\SPA\LoginUsecase;
 use App\Usecases\Auth\SPA\LogoutUsecase;
+use App\Usecases\Auth\SPA\RegisterUsecase;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class SPAAuthController extends Controller
 {
+    public function register(RegisterRequest $request, RegisterUsecase $usecase): JsonResponse
+    {
+        return $usecase($request);
+    }
+
     public function login(LoginRequest $request, LoginUsecase $usecase): JsonResponse
     {
         return $usecase($request);
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Auth\SPAAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/spa')->name('spa.')->group(function () {
+    Route::post('/register', [SPAAuthController::class, 'register'])->name('register');
     Route::post('/login', [SPAAuthController::class, 'login'])->middleware('throttle:10,1')->name('login');
 
     Route::middleware('auth:sanctum')->group(function () {
