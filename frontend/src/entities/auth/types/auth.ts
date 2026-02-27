a5ac43ff1e4f7f1493293a41b06db05227f973f1
s19013 <hideya670@gmail.com>
2026-02-27 17:53:30 +0900

    RegisterCredentials追加

--- frontend/src/entities/auth/types/auth.ts
+++ frontend/src/entities/auth/types/auth.ts
@@ -3,3 +3,5 @@ import type { components } from '@/shared/types/schema'
 export type User = components['schemas']['MinimumUserResource']
 
 export type LoginCredentials = components['schemas']['LoginRequest']
+
+export type RegisterCredentials = components['schemas']['RegisterRequest']
