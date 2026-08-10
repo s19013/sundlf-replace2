774526f4225bc4bd8c30558bf4f4a05bc381ca69
s19013 <hideya670@gmail.com>
2026-08-10 12:40:35 +0900

    物理削除するので、論理削除カラムは削除

--- backend/app/Models/Tag.php
+++ backend/app/Models/Tag.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $name
  * @property int $user_id
  * @property int $count
- * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  */
@@ -38,7 +37,6 @@ class Tag extends Model
     {
         return [
             'count' => 'integer',
-            'deleted_at' => 'datetime',
         ];
     }
 
