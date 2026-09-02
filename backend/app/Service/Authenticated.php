de9508fe48e69bb1a6aa78c9cf84cd4760148f45
s19013 <hideya670@gmail.com>
2026-09-03 00:30:47 +0900

    認証済みユーザーを取得するファサード作成

---
+++ backend/app/Service/Authenticated.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Service;
+
+use App\Exceptions\UnauthenticatedException;
+use App\Models\User;
+use Auth;
+
+class Authenticated
+{
+    public function user(): User
+    {
+        /** @var User|null $user */
+        $user = Auth::user();
+
+        if ($user === null) {
+            throw new UnauthenticatedException;
+        }
+
+        return $user;
+    }
+}
