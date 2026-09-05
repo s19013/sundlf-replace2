8d62df97cb619c68260796836cb86729d0df6648
s19013 <hideya670@gmail.com>
2026-09-05 16:34:07 +0900

    重複確認を後ろに回した

--- backend/app/Usecases/Tag/UpdateTagUsecase.php
+++ backend/app/Usecases/Tag/UpdateTagUsecase.php
@@ -2,18 +2,18 @@
 
 namespace App\Usecases\Tag;
 
-use App\Exceptions\DuplicationException;
 use App\Facades\Authenticated;
 use App\Http\Requests\Tag\UpdateTagRequest;
 use App\Models\Tag;
-use App\Models\User;
 use App\Usecases\Concerns\AssertOwner;
+use App\Usecases\Concerns\EnsureNotExists;
 use App\Usecases\Concerns\FindsModelOrFail;
 use Illuminate\Http\JsonResponse;
 
 class UpdateTagUsecase
 {
     use AssertOwner;
+    use EnsureNotExists;
     use FindsModelOrFail;
 
     public function __invoke(UpdateTagRequest $request): JsonResponse
@@ -23,11 +23,11 @@ class UpdateTagUsecase
         $id = (int) $request->validated('id');
         $newName = $request->string('name')->toString();
 
-        $this->checkDuplication($user, $newName, $id);
-
         $tag = $this->findOrFail(Tag::class, $id, '更新に失敗しました。');
 
         $this->assertOwner($tag, $user->id, 'このタグは更新できません。');
+
+        $this->ensureTagNotExists($user, $newName, $id);
 
         $oldName = $tag->name;
 
@@ -36,18 +36,5 @@ class UpdateTagUsecase
         return response()->json([
             'messages' => ["{$oldName}を{$newName}に更新しました。"],
         ]);
-    }
-
-    // HACK: これすでに存在しているかを確認してるから関数名不適切だよな
-    private function checkDuplication(User $user, string $newName, int $id): void
-    {
-        $duplicated = $user->tags()
-            ->where('name', $newName)
-            ->where('id', '!=', $id)
-            ->exists();
-
-        if ($duplicated) {
-            throw new DuplicationException($newName);
-        }
     }
 }
