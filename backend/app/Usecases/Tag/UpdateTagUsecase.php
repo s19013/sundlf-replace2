e422dce18b450a54c8829c04aca09be0c5b2b07c
s19013 <hideya670@gmail.com>
2026-09-05 16:48:43 +0900

    自分自身を除くか、全体をみるかで意味が違うため共通化はやめる

--- backend/app/Usecases/Tag/UpdateTagUsecase.php
+++ backend/app/Usecases/Tag/UpdateTagUsecase.php
@@ -2,18 +2,18 @@
 
 namespace App\Usecases\Tag;
 
+use App\Exceptions\DuplicationException;
 use App\Facades\Authenticated;
 use App\Http\Requests\Tag\UpdateTagRequest;
 use App\Models\Tag;
+use App\Models\User;
 use App\Usecases\Concerns\AssertOwner;
-use App\Usecases\Concerns\EnsureNotExists;
 use App\Usecases\Concerns\FindsModelOrFail;
 use Illuminate\Http\JsonResponse;
 
 class UpdateTagUsecase
 {
     use AssertOwner;
-    use EnsureNotExists;
     use FindsModelOrFail;
 
     public function __invoke(UpdateTagRequest $request): JsonResponse
@@ -27,7 +27,7 @@ class UpdateTagUsecase
 
         $this->assertOwner($tag, $user->id, 'このタグは更新できません。');
 
-        $this->ensureTagNotExists($user, $newName, $id);
+        $this->ensureNotExistsExceptSelf($user, $newName, $id);
 
         $oldName = $tag->name;
 
@@ -36,5 +36,18 @@ class UpdateTagUsecase
         return response()->json([
             'messages' => ["{$oldName}を{$newName}に更新しました。"],
         ]);
+    }
+
+    // 自分自身を除いて重複がないか
+    private function ensureNotExistsExceptSelf(User $user, string $newName, int $id): void
+    {
+        $duplicated = $user->tags()
+            ->where('name', $newName)
+            ->where('id', '!=', $id) // 自分を除外するためこの条件は必要
+            ->exists();
+
+        if ($duplicated) {
+            throw new DuplicationException($newName);
+        }
     }
 }
